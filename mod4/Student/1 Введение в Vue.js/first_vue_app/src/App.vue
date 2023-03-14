89425d050e492302001d63cb6e6134e683e0c982
q10sn1k <kondratenkona23@gmail.com>
2023-03-15 02:43:08 +0300

    bug fix

---
+++ mod4/Student/1 Введение в Vue.js/first_vue_app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+    <script>
+      export default {
+        data() {
+          return {
+            message: 'My First Vue App'
+          }
+        }
+      }
+    </script>
+
+  <style>
+    h1 {
+    color: red;
+  }
+  </style>
